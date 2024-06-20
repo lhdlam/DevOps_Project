9204f76142f86308b51f8a0e6ddc24de1348aee6
lhdlam <lam.lhd@mor.com.vn>
2024-06-20 13:34:40 +0700

    change instance

--- Terraform/project/project-example/main.tf
+++ Terraform/project/project-example/main.tf
@@ -87,7 +87,7 @@ module "nat_gateway" {
 
 
 resource "aws_instance" "dev_machine" {
-  ami = data.aws_ami.amz2023.id
+  ami = data.aws_ami.ubuntu2023
   instance_type = "t2.micro"
   key_name = "aws-key-pair"
   subnet_id     = module.vpc.public_sn_az1.id
